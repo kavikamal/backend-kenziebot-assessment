be1b0bdf9849d1e39b0e6742fbcf99d16259176d
Kavitha Kamal <kavithakamal@gmail.com>
2018-09-16 22:09:08 -0400

    Updated code

--- tests/test_kbot.py
+++ tests/test_kbot.py
@@ -1,14 +1,13 @@
 import unittest
-import kbot.py
+import kbot
 
 class TestSlackbot(unittest.TestCase):
-
-    def test_auth(self):
-        self.assertEqual(slack_client.api_call("auth.test").get("ok"), True)
-
-
-    def test_handle_command('choc','DCR9ET3FD'):
-        self.assertTrue(':choclate:')
+    def test_handle_command(self):
+        "Tests the handle command functions inputs"
+        self.assertEqual(
+            kbot.handle_command('choc', "channel"),
+            ":chocolate_bar:")
+    
 
 
 if __name__ == '__main__':
